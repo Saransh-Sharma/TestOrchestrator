35abc9d97bc36532b92a5b3d7e2053a2d8fac1e2
Saransh Sharma <saransh1337@gmail.com>
2018-12-06 15:27:26 +0530

    Create Version.py
    add python version check

---
+++ settings/Version.py
@@ -0,0 +1,26 @@
+import sys
+
+from exceptions.Exceptions import LauncherFlowInterruptedException
+
+from system.console import (
+    Printer,
+    Color
+)
+
+NUMBER = "1.1-beta"
+MIN_PYTHON_VER = (3, 6)
+
+
+def info():
+    Printer.system_message("", "TestOrchestrator version: " + Color.GREEN + NUMBER + Color.BLUE + ".")
+
+
+def python_check():
+    if sys.version_info >= MIN_PYTHON_VER:
+        Printer.system_message("", "Minimum Python version requirement met! Your version: " + Color.GREEN
+                               + str(sys.version_info) + Color.BLUE + ".")
+
+    else:
+        message = ("Invalid Python version. Please use at least Python " + str(MIN_PYTHON_VER[0]) + "."
+                   + str(MIN_PYTHON_VER[1]) + ".")
+        raise LauncherFlowInterruptedException("", message)
