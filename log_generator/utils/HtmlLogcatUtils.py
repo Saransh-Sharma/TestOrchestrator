6c17c4680e81eeeb974cec0ca0dfd0898e4cb292
Saransh Sharma <saransh1337@gmail.com>
2018-12-17 13:24:51 +0530

    add utils to generate HTML report

---
+++ log_generator/utils/HtmlLogcatUtils.py
@@ -0,0 +1,70 @@
+def add_logcat_header(text):
+    return "<div class=\"logcat-header\">{}</div>".format(text)
+
+
+def start_logcat_table():
+    return "<div class=\"logcat-table\">"
+
+
+def add_debug_row(date, time, tag, level, message):
+    row_content = ""
+    row_content += "<div class=\"logcat-debug-row\">"
+    row_content += "<div class=\"cell centered-cell date-cell\">{}</div>".format(date)
+    row_content += "<div class=\"cell centered-cell\">{}</div>".format(time)
+    row_content += "<div class=\"cell centered-cell\">{}</div>".format(tag)
+    row_content += "<div class=\"cell centered-cell\">{}</div>".format(level)
+    row_content += "<div class=\"cell message-cell\">{}</div>".format(message)
+    row_content += "</div>"
+    return row_content
+
+
+def add_verbose_row(date, time, tag, level, message):
+    row_content = ""
+    row_content += "<div class=\"logcat-verbose-row\">"
+    row_content += "<div class=\"cell centered-cell date-cell\">{}</div>".format(date)
+    row_content += "<div class=\"cell centered-cell\">{}</div>".format(time)
+    row_content += "<div class=\"cell centered-cell\">{}</div>".format(tag)
+    row_content += "<div class=\"cell centered-cell\">{}</div>".format(level)
+    row_content += "<div class=\"cell message-cell\">{}</div>".format(message)
+    row_content += "</div>"
+    return row_content
+
+
+def add_info_row(date, time, tag, level, message):
+    row_content = ""
+    row_content += "<div class=\"logcat-info-row\">"
+    row_content += "<div class=\"cell centered-cell date-cell\">{}</div>".format(date)
+    row_content += "<div class=\"cell centered-cell\">{}</div>".format(time)
+    row_content += "<div class=\"cell centered-cell\">{}</div>".format(tag)
+    row_content += "<div class=\"cell centered-cell\">{}</div>".format(level)
+    row_content += "<div class=\"cell message-cell\">{}</div>".format(message)
+    row_content += "</div>"
+    return row_content
+
+
+def add_warning_row(date, time, tag, level, message):
+    row_content = ""
+    row_content += "<div class=\"logcat-warning-row\">"
+    row_content += "<div class=\"cell centered-cell date-cell\">{}</div>".format(date)
+    row_content += "<div class=\"cell centered-cell\">{}</div>".format(time)
+    row_content += "<div class=\"cell centered-cell\">{}</div>".format(tag)
+    row_content += "<div class=\"cell centered-cell\">{}</div>".format(level)
+    row_content += "<div class=\"cell message-cell\">{}</div>".format(message)
+    row_content += "</div>"
+    return row_content
+
+
+def add_error_row(date, time, tag, level, message):
+    row_content = ""
+    row_content += "<div class=\"logcat-exceptions-row\">"
+    row_content += "<div class=\"cell centered-cell date-cell\">{}</div>".format(date)
+    row_content += "<div class=\"cell centered-cell\">{}</div>".format(time)
+    row_content += "<div class=\"cell centered-cell\">{}</div>".format(tag)
+    row_content += "<div class=\"cell centered-cell\">{}</div>".format(level)
+    row_content += "<div class=\"cell message-cell\">{}</div>".format(message)
+    row_content += "</div>"
+    return row_content
+
+
+def end_table():
+    return "</div>"
